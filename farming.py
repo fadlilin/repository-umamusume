593cf968203d3a4843396a64a7bac6312b274335
fadlilin <fairulfadli10@gmail.com>
2026-02-09 20:25:14 +0700

    game farming, blm selesai

---
+++ farming.py
@@ -0,0 +1,57 @@
+import os
+import status
+import uang
+
+from status import Status
+status = Status()
+
+from uang import uang
+money = uang()
+
+class Player:
+    def __init__(self):
+        self.day = status.day
+        self.stamina = status.stamina
+        self.maxstamina = status.max_stamina
+        self.uang = money.saldo
+
+class Crop:
+    def __init__(self):
+        pass
+
+class Farm:
+    def __init__(self):
+        pass
+
+class Game:
+    def __init__(self):
+        self.running = True
+        self.player = Player()
+
+    def farmMenu(self):
+        os.system("cls")
+        print("==========Farm Game==========")
+        print(f"day\t:{self.player.day}")
+        print(f"duit\t:{self.player.uang}")
+        print(f"stamina\t:{self.player.stamina}")
+        print("=============================")
+        print("1. Tanam\n2. Panen\n3. Siram\n4. Inventory\n5. Shop")
+        print("=============================")
+        print("0. Kembali?")
+        pilih = input("> ")
+
+        if pilih == "1":
+            pass
+        elif pilih == "2":
+            pass
+        elif pilih == "3":
+            pass
+        elif pilih == "4":
+            pass
+        elif pilih == "5":
+            pass
+        elif pilih == "0":
+            pass
+
+game = Game()
+game.farmMenu()
